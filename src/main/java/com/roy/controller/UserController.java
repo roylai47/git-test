774a420a9ff8aa166ef993e8c6f14ec8846811ab
roylai47 <836314020@qq.com>
2021-07-11 11:04:49 +0800

    commit

---
+++ src/main/java/com/roy/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.roy.controller;
+
+/***
+ * @ClassName: UserController
+ * @Description:
+ * @Auther: lairf
+ * @Date: 2021/7/11  11:03
+ * @version : 1.0
+ */
+public class UserController {
+}
